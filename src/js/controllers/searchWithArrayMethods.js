b0e53dddb73ff0956fd082041426295ed8de7275
thealamenthed <dalila.boualemfr@gmail.com>
2025-05-15 16:08:37 +0200

    docs(search): add comments

--- src/js/controllers/searchWithArrayMethods.js
+++ src/js/controllers/searchWithArrayMethods.js
@@ -23,20 +23,23 @@ export function updateResults({
   const rawQuery = searchInput.value.trim();
   const q = normalize(rawQuery);
 
+  // On filtre les recettes par nom et description
   const textFiltered =
     q.length >= 3
       ? recipes.filter((r) =>
           (normalize(r.name) + " " + normalize(r.description)).includes(q)
         )
-      : [...recipes];
+      : [...recipes]; // Si la recherche est inférieure à 3 caractères, on affiche toutes les recettes
 
   const tagFiltered = Object.entries(selectedTags).reduce(
-    (acc, [type, tags]) => (tags.length ? filterByTags(acc, tags, type) : acc),
-    textFiltered
+    // On filtre les recettes par tags
+    (acc, [type, tags]) => (tags.length ? filterByTags(acc, tags, type) : acc), // si la liste tags n’est pas vide, on appelle filterByTags(acc, tags, type), sinon on passe simplement l’accumulateur tel quel
+    textFiltered //  liste des recettes filtrées à la fois par texte et par tags
   );
 
   const ingredients = getUniqueIngredients(tagFiltered).filter(
-    (i) => !selectedTags.ingredient.includes(i)
+    // Extrait l’ensemble des tags disponibles dans le résultat
+    (i) => !selectedTags.ingredient.includes(i) //puis on enlève ceux déjà sélectionné pour ne pas les proposer à nouveau dans le dropdown
   );
   const appliances = getUniqueAppliances(tagFiltered).filter(
     (a) => !selectedTags.appliance.includes(a)
@@ -54,9 +57,10 @@ export function updateResults({
   initFilterListeners();
 
   const count = tagFiltered.length;
-  recipesCountEl.textContent = `${count} recette${count > 1 ? "s" : ""}`;
+  recipesCountEl.textContent = `${count} recette${count > 1 ? "s" : ""}`; //Mise à jour du compteur
 
   if (count === 0 && rawQuery.length > 0) {
+    // Si aucune recette ne correspond à la recherche
     const suggestion =
       recipes.length > 0 ? recipes[0].name : "une autre recherche";
     cardsContainer.innerHTML = `
@@ -70,5 +74,5 @@ export function updateResults({
     renderRecipes(tagFiltered, cardsContainer);
   }
 
-  renderSelectedTags(selectedTagsContainer, selectedTags);
+  renderSelectedTags(selectedTagsContainer, selectedTags); // Affichage des tags sélectionnés
 }
